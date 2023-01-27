050212d0f31844a0d79fe924d74127c3d336bcb5
akulackovan <32873566+akulackovan@users.noreply.github.com>
2023-01-27 21:20:18 +0300

    Add loading and change button to svg

--- new server/client/src/components/PostPage/PostPageComponent.jsx
+++ new server/client/src/components/PostPage/PostPageComponent.jsx
@@ -4,12 +4,14 @@ import { positions } from 'react-alert'
 import { Redirect, Link } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext'
 import './PostPage.scss'
+import ReactLoading from 'react-loading';
 
-
-const PostPageComponent = ({ id, idUser }) => {
+const PostPageComponent = ({ id }) => {
 
     const [post, setPost] = useState("")
-    const [city, setCity] = useState('')
+    const [loading, setLoading] = useState(true)
+
+    const [loadingComm, setLoadingComm] = useState(true)
     const [error, setErrorMessage] = useState("")
     const [comment, setComment] = useState("")
     const [comments, setComments] = useState(null)
@@ -35,7 +37,6 @@ const PostPageComponent = ({ id, idUser }) => {
                 console.log(like)
             }).catch(error => {
                 setErrorMessage(error.response.data.message)
-
                 setTimeout(() => setErrorMessage(""), 2000)
             })
     }
@@ -55,9 +56,11 @@ const PostPageComponent = ({ id, idUser }) => {
                 console.log(response.data)
                 if (response.data.total.length == 0) {
                     setComments(null)
+                    setLoadingComm(false)
                     return
                 }
                 setComments(response.data.total)
+                setLoadingComm(false)
             }).catch(error => {
                 setErrorMessage(error.response.data.message)
                 setTimeout(() => setErrorMessage(""), 2000)
@@ -79,8 +82,10 @@ const PostPageComponent = ({ id, idUser }) => {
                 console.log(response.data.isPost)
                 setPost(response.data.isPost)
                 console.log(post)
-                getComments()
                 getLike()
+                setLoading(false)
+                getComments()
+
                 post.timestamps = post.timestamps.split('-')
                 console.log(post.timestamps)
 
@@ -105,7 +110,6 @@ const PostPageComponent = ({ id, idUser }) => {
                 .then(response => {
                     console.log(response.data)
                     setView(true)
-
                 }).catch(error => {
                     setErrorMessage(error.response.data.message)
                     setTimeout(() => setErrorMessage(""), 2000)
@@ -137,17 +141,17 @@ const PostPageComponent = ({ id, idUser }) => {
                     setLike(response.data.like)
                 })
 
-                await axios({
-                    method: 'get',
-                    url: '/post/updateLike',
-                    params: {
-                        'idPost': id
-                    }
+            await axios({
+                method: 'get',
+                url: '/post/updateLike',
+                params: {
+                    'idPost': id
+                }
+            })
+                .then(response => {
+                    setPost({ ...post, likes: response.data.likes })
+                    console.log(response)
                 })
-                    .then(response => {
-                        setPost({...post, likes: response.data.likes})
-                        console.log(response)
-                    })
         }
         catch (error) {
             console.log(error)
@@ -157,7 +161,7 @@ const PostPageComponent = ({ id, idUser }) => {
     }
 
     const commentHandler = async () => {
-
+        setLoadingComm(true)
         try {
             await axios({
                 method: 'post',
@@ -184,11 +188,18 @@ const PostPageComponent = ({ id, idUser }) => {
         }
     }
 
+
+    if (loading) {
+        return (
+            <ReactLoading type='spinningBubbles' height={'20%'} width={'20%'} />
+        )
+    }
+
     return (
         <div className='allPost'>
             {error && <h6>Ошибка: {error}</h6>}
             {post &&
-                <div className='back'>
+                <div className='post'>
                     <div className='one'>
                         <div >
                             <div className='user'>
@@ -198,27 +209,35 @@ const PostPageComponent = ({ id, idUser }) => {
                             </div>
                             <div className='under'>
                                 <div className='data'>
-                                    {post.timestamps[8]}{post.timestamps[9]}\{post.timestamps[5]}{post.timestamps[6]}\{post.timestamps[2]}{post.timestamps[3]} &emsp; г.{post.city.city}
+                                    {post.timestamps[8]}{post.timestamps[9]}\{post.timestamps[5]}{post.timestamps[6]}\{post.timestamps[2]}{post.timestamps[3]}&emsp;г.{post.city.city}
                                 </div>
                             </div>
                         </div>
-                        <img className='img' style={{ width: '738px', height: '590px' }} src={`${post.image}`} />
+                        <img className='img' style={{ width: '600px', height: '479px' }} src={`${post.image}`} />
                         <div className='stat'>
                             <div>
-                            Просмотры: {post.views} &emsp;&emsp;&emsp;Лайки: {post.likes}&emsp;&emsp;&emsp; Комментарии: {post.comments.length}
+                                Просмотры: {post.views} &emsp;&emsp;&emsp;Лайки: {post.likes}&emsp;&emsp;&emsp; Комментарии: {post.comments.length}
                             </div>
 
                         </div>
-                        <div className='text' style={{width: '80%'}}>
-                        <h5>Описание</h5>
-                            {post.text}
+                        <div className='text'>
+                            <h5>Описание</h5>
+                            <div>{post.text}</div>
                         </div>
                     </div>
                     <div className='second'>
-                    <div className='comment'>
-                    {!like && <button className='button like' onClick={changeLike} >ЛАЙК</button>}
-                            {like && <button className='button dislike' onClick={changeLike}>ОТМЕНИТЬ ЛАЙК</button>}
-                        
+                        <div className='comment'>
+                            {!like && <button className='like' onClick={changeLike} >
+                                <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none">
+                                    <path className='like' d="M16.44 3.10156C14.63 3.10156 13.01 3.98156 12 5.33156C10.99 3.98156 9.37 3.10156 7.56 3.10156C4.49 3.10156 2 5.60156 2 8.69156C2 9.88156 2.19 10.9816 2.52 12.0016C4.1 17.0016 8.97 19.9916 11.38 20.8116C11.72 20.9316 12.28 20.9316 12.62 20.8116C15.03 19.9916 19.9 17.0016 21.48 12.0016C21.81 10.9816 22 9.88156 22 8.69156C22 5.60156 19.51 3.10156 16.44 3.10156Z" />
+                                </svg>
+                            </button>}
+                            {like && <button className='dislike' onClick={changeLike}>
+                                <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none">
+                                    <path className='dislike' d="M16.44 3.10156C14.63 3.10156 13.01 3.98156 12 5.33156C10.99 3.98156 9.37 3.10156 7.56 3.10156C4.49 3.10156 2 5.60156 2 8.69156C2 9.88156 2.19 10.9816 2.52 12.0016C4.1 17.0016 8.97 19.9916 11.38 20.8116C11.72 20.9316 12.28 20.9316 12.62 20.8116C15.03 19.9916 19.9 17.0016 21.48 12.0016C21.81 10.9816 22 9.88156 22 8.69156C22 5.60156 19.51 3.10156 16.44 3.10156Z" />
+                                </svg>
+                            </button>}
+
                             <form id='inputs'>
                                 <textarea
                                     className="textarea"
@@ -230,12 +249,18 @@ const PostPageComponent = ({ id, idUser }) => {
                                 />
                             </form>
                             <div onClick={commentHandler}>
-                                <button className='button'>Опубликовать</button>
+                                <button className='send'>
+                                    <svg className='svg' width="50px" height="50px" viewBox="0 0 24 24" fill="red">
+                                        <path className='send' d="M18.0693 8.50867L9.50929 4.22867C3.75929 1.34867 1.39929 3.70867 4.27929 9.45867L5.14929 11.1987C5.39929 11.7087 5.39929 12.2987 5.14929 12.8087L4.27929 14.5387C1.39929 20.2887 3.74929 22.6487 9.50929 19.7687L18.0693 15.4887C21.9093 13.5687 21.9093 10.4287 18.0693 8.50867ZM14.8393 12.7487H9.43929C9.02929 12.7487 8.68929 12.4087 8.68929 11.9987C8.68929 11.5887 9.02929 11.2487 9.43929 11.2487H14.8393C15.2493 11.2487 15.5893 11.5887 15.5893 11.9987C15.5893 12.4087 15.2493 12.7487 14.8393 12.7487Z" fill="#292D32" />
+                                    </svg>
+
+
+                                </button>
                             </div>
-                            {comments== 0 && <h3>Комментариев нет</h3>}
+                            {comments == 0 && <h3>Комментариев нет</h3>}
                         </div>
-                        
-                        {comments  && <div className='comments'>
+                        {loadingComm && <ReactLoading type='spinningBubbles' height={'20%'} width={'20%'} />}
+                        {comments && <div className='comments'>
                             <ul>
                                 {comments.map(item => (
                                     <li>
