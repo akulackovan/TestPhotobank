0227b4506a168b002138ec228e6bfd8f2a090b30
akulackovan <akulackovan@gmail.com>
2023-01-30 23:19:21 +0700

    Change format for NavBar.jsx

--- new server/client/src/components/NavBar/NavBar.jsx
+++ new server/client/src/components/NavBar/NavBar.jsx
@@ -1,60 +1,65 @@
-import React, { useState } from 'react'
-import { Redirect } from 'react-router-dom'
-import './NavBar.scss'
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import "./NavBar.scss";
 
 const NavBar = () => {
-    const [form, setForm] = useState("")
-    const [str, setStr] = useState("")
+  const [form, setForm] = useState("");
+  const [str, setStr] = useState("");
 
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            setStr('/search/' + form)
-        }
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      setStr("/search/" + form);
     }
+  };
 
-    const changeForm = (event) => {
-        setForm(event.target.value)
-    }
+  const changeForm = (event) => {
+    setForm(event.target.value);
+  };
 
-    if (str) {
-        return (<Redirect to={str} />)
-    }
+  if (str) {
+    return <Redirect to={str} />;
+  }
 
-    return (
-        <div className='nav-container'>
-            <div className='center'>
-            <div className="nav">
-                
-                <div className='logo-text'>ФОТОБАНК
-                </div>
+  return (
+    <div className="nav-container">
+      <div className="center">
+        <div className="nav">
+          <div className="logo-text">ФОТОБАНК</div>
 
-
-                {/* Список ссылок по требованиям к navbar */}
-                <div className='nav-elements'>
-                    <div className='center'>
-                    <ul className="list">
-                        <li><a href='/popular'>Популярное</a></li>
-                        <li><a href='/subsc'>Подписки</a></li>
-                        <li><a href='/profile'>Профиль</a></li>
-                        <li><a href='/settings'>Настройки</a></li>
-                        <li>
-                            <input
-                                className="input"
-                                type="text"
-                                placeholder="ПОИСК"
-                                name="user"
-                                onKeyPress={handleKeyPress}
-                                onChange={changeForm}
-                                width='10'
-                            />
-                        </li>
-                    </ul>
-                    </div>
-                </div>
-                </div>
+          {/* Список ссылок по требованиям к navbar */}
+          <div className="nav-elements">
+            <div className="center">
+              <ul className="list">
+                <li>
+                  <a href="/popular">Популярное</a>
+                </li>
+                <li>
+                  <a href="/subsc">Подписки</a>
+                </li>
+                <li>
+                  <a href="/profile">Профиль</a>
+                </li>
+                <li>
+                  <a href="/settings">Настройки</a>
+                </li>
+                <li>
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="ПОИСК"
+                    name="user"
+                    onKeyPress={handleKeyPress}
+                    onChange={changeForm}
+                    width="10"
+                  />
+                </li>
+              </ul>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
