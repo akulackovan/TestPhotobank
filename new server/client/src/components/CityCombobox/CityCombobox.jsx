322e42c894fe8f20c1c78879877bdee251aae02d
akulackovan <32873566+akulackovan@users.noreply.github.com>
2023-02-01 22:54:17 +0300

    Add comments css and change

--- new server/client/src/components/CityCombobox/CityCombobox.jsx
+++ new server/client/src/components/CityCombobox/CityCombobox.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
+import './CityCombobox.scss'
 
 const CityCombobox = ({ onChange }) => {
   const [showMenu, setShowMenu] = useState(false);
