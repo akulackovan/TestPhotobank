575c8e4fdf5dee0ea292e8b05c106b3ec2ea279b
akulackovan <32873566+akulackovan@users.noreply.github.com>
2023-02-15 14:13:08 +0300

    Changes

--- new server/client/src/components/Cropper/cropper.jsx
+++ new server/client/src/components/Cropper/cropper.jsx
@@ -5,8 +5,9 @@ import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 import "react-image-crop/dist/ReactCrop.css";
 import "react-image-crop/src/ReactCrop.scss";
+import { set } from "mongoose";
 
-const Cropper = ({ x = 480, y = 480, size = 7, setData, isOut }) => {
+const Cropper = ({ x = 480, y = 480, size = 7, setData}) => {
   const [info, setInfo] = useState({
     x: null,
     y: null,
@@ -24,6 +25,7 @@ const Cropper = ({ x = 480, y = 480, size = 7, setData, isOut }) => {
   const selectImage = (e) => {
     setErrorMessage(null);
     setOutput(null);
+    setData(false)
     setSrc(null);
     const temp = URL.createObjectURL(e.target.files[0]);
     var img = new Image();
@@ -117,8 +119,8 @@ const Cropper = ({ x = 480, y = 480, size = 7, setData, isOut }) => {
           y: 0,
         });
       }
+      setData(true)
     };
-    isOut(true)
   };
 
   /** Обрезка изображения */
@@ -160,8 +162,7 @@ const Cropper = ({ x = 480, y = 480, size = 7, setData, isOut }) => {
     setSrc(null);
     setData(base64Image);
     console.log(base64Image);
-    
-    isOut(false)
+  
   };
 
   const ChangeCrop = () => {
