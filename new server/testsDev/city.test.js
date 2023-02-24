55f8f0534d600e033ca8ce6694424e6582977c0d
akulackovan <32873566+akulackovan@users.noreply.github.com>
2023-02-24 22:45:53 +0300

    Change city

--- new server/testsDev/city.test.js
+++ new server/testsDev/city.test.js
@@ -1,102 +1,77 @@
 /**
  * @jest-environment node
  */
-
-import mongoose from "mongoose";
-import express from "express";
-import request from 'supertest';
-import router from '../router/city.js';
-import dotenv from 'dotenv'
-import cors from 'cors'
-
-const app = express();
-const PORT = process.env.PORT || 8015
-
-app.use(cors());
-app.use(express.json())
-
-app.use('/city', router)
-
-app.listen(PORT, () => {
-  console.log("Start server on port", PORT)})
+import { getCity, getAllCity } from "../controllers/city.js";
+import City from "../models/City.js";
+jest.mock("../models/City.js");
 
 describe("GET /city/getAllCity", () => {
-  /* Каждый раз присоединяемся к БД */
-  beforeEach(async () => {
-    await mongoose.connect(
-      `mongodb+srv://admin:admin@test.qidx0uu.mongodb.net/?retryWrites=true&w=majority`
-    );
-    dotenv.config()
-  })
-
-  /* Закрываем БД */
-  afterEach(async () => {
-    await mongoose.connection.close();
-  });
 
 
   it("Should return all city", async () => {
-    const res = await request(app).get('/city/getallcity')
-    expect(res.statusCode).toEqual(200)
-    expect(res.body.city.length).toBeGreaterThan(1000);
-    expect(res.body.message).toBe("Города получены")
-    expect(res.body.city[0]._id).not.toBeNull()
-    expect(res.body.city[0].city).not.toBeNull()
-  });
+    const req = {}
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
 
-  it("Should return error with close mongoose", async () => {
-    await mongoose.connection.close();
-    const res = await request(app).get(`/city/getallcity`)
-    expect(res.statusCode).toEqual(400)
-    expect(res.body.message).toBe("Ошибка при получении городов")
+    City.find = jest.fn().mockResolvedValue([
+      { _id: "63b9473e70bfa1abe160400f", city: "Москва" },
+      { _id: "63b94e8d70bfa1abe1604015", city: "Санкт-Петербург" },
+      { _id: "63b96121b19de65ebdf4cd50", city: "Абаза" },
+    ]);
+
+    await getAllCity(req, res);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "Города получены",
+      city: [
+        { _id: "63b9473e70bfa1abe160400f", city: "Москва" },
+        { _id: "63b94e8d70bfa1abe1604015", city: "Санкт-Петербург" },
+        { _id: "63b96121b19de65ebdf4cd50", city: "Абаза" },
+      ]
+    });
   });
 
 });
 
 
 describe("GET /city/getcity", () => {
-  /* Каждый раз присоединяемся к БД */
-  beforeEach(async () => {
-    await mongoose.connect(
-      `mongodb+srv://admin:admin@test.qidx0uu.mongodb.net/?retryWrites=true&w=majority`
-    );
-  dotenv.config()
-  })
-
-  /* Закрываем БД */
-  afterEach(async () => {
-    await mongoose.connection.close();
-  });
-
+ 
   it("Should return city with correct id", async () => {
-    const id = "63b9473e70bfa1abe160400f"
-    const res = await request(app).get(`/city/getcity?cityId=${id}`)
-    expect(res.statusCode).toEqual(200)
-    expect(res.body.message).toBe("Город найден")
-    expect(res.body.isCity._id).not.toBeNull()
-    expect(res.body.isCity._id).toBe(id)
-    expect(res.body.isCity.city).not.toBeNull()
+    const req = { query: { cityId: "1234"} };
+
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    City.findOne = jest.fn().mockResolvedValue(
+      { _id: "63b9473e70bfa1abe160400f", city: "Москва" },
+      );
+
+     await getCity(req, res)
+
+     expect(res.json).toHaveBeenCalledWith({
+      message: 'Город найден',
+      isCity: { _id: "63b9473e70bfa1abe160400f", city: "Москва" }
+    });
   });
 
   it("Should return error with random string", async () => {
-    const res = await request(app).get(`/city/getcity?cityId=63b9`)
-    expect(res.statusCode).toEqual(400)
-    expect(res.body.message).toBe("Ошибка при получении города")
+    const req = { query: { cityId: "1234"} };
+
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    City.findOne = jest.fn().mockResolvedValue(
+     null);
+
+     await getCity(req, res)
+
+     expect(res.json).toHaveBeenCalledWith({
+      message: 'Города нет',
+    });
   });
-
-
-  it("Should return error with unknown city", async () => {
-    const res = await request(app).get(`/city/getcity?cityId=66b9473e70bfa1abe160400f`)
-    expect(res.statusCode).toEqual(400)
-    expect(res.body.message).toBe("Города нет")
-  });
-
-  it("Should return error with unknown city", async () => {
-    await mongoose.connection.close();
-    const res = await request(app).get(`/city/getcity?cityId=66b9473e70bfa1abe160400f`)
-    expect(res.statusCode).toEqual(400)
-    expect(res.body.message).toBe("Ошибка при получении города")
-  });
-
-
 })
