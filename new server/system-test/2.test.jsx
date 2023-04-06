dd09e44d3bfac98831f8627ed49a91149bef1fbc
akulackovan <32873566+akulackovan@users.noreply.github.com>
2023-04-06 23:19:40 +0300

    Add 2

---
+++ new server/system-test/2.test.jsx
@@ -0,0 +1,35 @@
+import puppeteer from "puppeteer";
+
+test("Wrong password", async () => {
+  const browser = await puppeteer.launch({
+    //headless: false, slowMo: 100, // Uncomment to visualize test
+  });
+  const page = await browser.newPage();
+
+  // Load "http://localhost:3000/auth"
+  await page.goto('http://localhost:3000/auth');
+
+  // Fill "test" on <input> [data-testid="username"]
+  await page.waitForSelector('[data-testid="username"]:not([disabled])');
+  await page.type('[data-testid="username"]', "test");
+
+  // Fill "test123" on <input> #password
+  await page.waitForSelector("#password:not([disabled])");
+  await page.type("#password", "test123");
+
+  // Click on <button> "ВОЙТИ"
+  await page.waitForSelector('[data-testid="login-button"]');
+  await page.click('[data-testid="login-button"]');
+
+    // Click on <p> ""Описание пользователя" д..."
+    await page.waitForSelector(".error");
+    //Проверка на сообщение
+    await page.$eval(
+      ".error",
+      (el) =>
+        (el.textContent =
+          'Неверный пароль')
+    );
+
+  await browser.close();
+});
